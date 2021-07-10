7d9a3b5bb34fd5d9d0be7e07f032b4aa5707cc93
NAWFAL DARIANE <DARIANE_NAWFAL@emsi-edu.ma>
2021-07-10 23:49:10 +0100

    back end cinema

---
+++ Application_Cinema/src/main/java/org/sid/Cinema/dao/CategorieRepository.java
@@ -0,0 +1,12 @@
+package org.sid.Cinema.dao;
+
+import org.sid.Cinema.entities.Categorie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin("*")
+public interface CategorieRepository extends JpaRepository<Categorie, Long> {
+
+}
